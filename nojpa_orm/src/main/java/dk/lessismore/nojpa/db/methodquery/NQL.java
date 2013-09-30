71b29b1b68dd9bf6cc705504bc8e3764ce8219c6
Atanas Balevsky <atanas.balevsky@gmail.com>
2013-09-30 15:17:54 +0300

    remove pipe | from search queries

--- nojpa_orm/src/main/java/dk/lessismore/nojpa/db/methodquery/NQL.java
+++ nojpa_orm/src/main/java/dk/lessismore/nojpa/db/methodquery/NQL.java
@@ -826,7 +826,7 @@ public class NQL {
         } else {
             s = s.replaceAll("\"", " ").replaceAll("!", " ").replaceAll("|", " ").replaceAll("'", " ").replaceAll("\\^", " ")
                     .replaceAll("$", " ").replaceAll("§", " ").replaceAll("#", " ").replaceAll(":", " ").replaceAll("_", " ")
-                    .replaceAll("/", " ").replaceAll(";", " ").replaceAll("€", " ").replaceAll("%", " ").replaceAll("/", " ")
+                    .replaceAll("/", " ").replaceAll(";", " ").replaceAll("€", " ").replaceAll("%", " ").replaceAll("/", " ").replaceAll("|", " ")
                     .replaceAll("\\?", " ").replaceAll("\\(", " ").replaceAll("\\)", " ").replaceAll("\\{", " ").replaceAll("\\}", " ")
                     .replaceAll("\\[", " ").replaceAll("\\]", " ")
                     .replaceAll("<", " ").replaceAll(">", " ").replaceAll("^", " ").replaceAll("~", " ").replaceAll("\\+", " ").replaceAll("-", " ")
