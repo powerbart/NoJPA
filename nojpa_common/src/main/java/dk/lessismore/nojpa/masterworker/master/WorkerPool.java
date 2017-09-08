a56a97890110cf25c6f32d6267a542c3b2eb52fe
Sebastian Berg <sebastian@less-is-more.dk>
2017-09-08 15:07:02 +0300

    Closing the ServerLink when unregisterWorker

--- nojpa_common/src/main/java/dk/lessismore/nojpa/masterworker/master/WorkerPool.java
+++ nojpa_common/src/main/java/dk/lessismore/nojpa/masterworker/master/WorkerPool.java
@@ -36,6 +36,9 @@ public class WorkerPool {
         WorkerEntry entry = pool.get(serverLink); 
         if (entry != null) {
             pool.remove(serverLink);
+            try {
+                serverLink.close();
+            } catch (Exception e){}
         }
     }
 
