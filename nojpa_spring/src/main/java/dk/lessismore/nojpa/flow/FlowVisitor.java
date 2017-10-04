f4aeae207aa04d17a1ae2dc4908adbb468b0e603
Sebastian Berg <sebastian@less-is-more.dk>
2017-10-04 17:47:20 +0300

    First version where we are using FlowVisitor :-)

--- nojpa_spring/src/main/java/dk/lessismore/nojpa/flow/FlowVisitor.java
+++ nojpa_spring/src/main/java/dk/lessismore/nojpa/flow/FlowVisitor.java
@@ -2,12 +2,17 @@ package dk.lessismore.nojpa.flow;
 
 import dk.lessismore.nojpa.db.methodquery.MQL;
 import dk.lessismore.nojpa.reflection.db.model.ModelObjectInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 public abstract class FlowVisitor<T extends ModelObjectInterface> {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
 
     private final ThreadPoolTaskExecutor executor;
+    int currentCount = 0;
 
     public FlowVisitor() {
         executor = new ThreadPoolTaskExecutor();
@@ -17,33 +22,72 @@ public abstract class FlowVisitor<T extends ModelObjectInterface> {
         executor.setMaxPoolSize(getNumberOfThreads());
     }
 
-    abstract int getNumberOfThreads();
+    protected abstract int getNumberOfThreads();
 
-    abstract int getMinimumQueueSize();
+    protected abstract int getMinimumQueueSize();
 
-    abstract int getSplitLimitSize();
+    protected abstract int getSplitLimitSize();
 
 
-    abstract String getThreadNamePrefix();
+    protected abstract String getThreadNamePrefix();
+
+    public int getCurrentQueueSize(){
+        return executor.getThreadPoolExecutor().getQueue().size();
+    }
 
     public void runFlow() {
-        int totalCount = query().getCount();
+        final int totalCount = getTotalCount();
 
-        int queueSize = executor.getThreadPoolExecutor().getQueue().size();
 
-        int currentCount = 0;
-        while (totalCount < getSplitLimitSize() + currentCount)
-            while (queueSize < getMinimumQueueSize()) {
-                // TODO do something with totalCount
-
-                query().limit(currentCount, getSplitLimitSize())
-                        .visit(new AbstractCountingVisitor<T>() {
+        while (getSplitLimitSize() + currentCount < totalCount) {
+            while (getCurrentQueueSize() < getMinimumQueueSize()) {
+                log.debug("WorkQueue-fill-up: getCurrentQueueSize("+ getCurrentQueueSize() +"), getMinimumQueueSize("+ getMinimumQueueSize() +")");
+                MQL.SelectQuery<T> query = query();
+                if(startFromBeginning()){
+                    query.limit(getSplitLimitSize());
+                } else {
+                    query.limit(currentCount, getSplitLimitSize());
+                }
+                query.visit(new AbstractCountingVisitor<T>() {
                             @Override
                             public void process(T t) {
-                                executor.execute(() -> process(t));
+                                currentCount++;
+                                executor.execute(() -> doWork(t));
                             }
                         });
             }
+            log.debug("WorkQueue-is-full: getCurrentQueueSize("+ getCurrentQueueSize() +"), getMinimumQueueSize("+ getMinimumQueueSize() +")");
+            int beforeQSize = getCurrentQueueSize();
+            int sameCount = 0;
+            for (int i = 0; i < 180; i++) {
+                try {
+                    Thread.sleep(1_000);
+                } catch (InterruptedException e) {
+                }
+                if(beforeQSize == getCurrentQueueSize()){
+                    log.debug("WorkQueue-has-not-changed: getCurrentQueueSize("+ getCurrentQueueSize() +"), getMinimumQueueSize("+ getMinimumQueueSize() +")");
+                    sameCount++;
+                }
+                log.debug("WorkQueue-checking: getCurrentQueueSize("+ getCurrentQueueSize() +"), getMinimumQueueSize("+ getMinimumQueueSize() +")");
+                if(getCurrentQueueSize() < getMinimumQueueSize()){
+                    break;
+                }
+                log.debug("WorkQueue-is-full: getCurrentQueueSize("+ getCurrentQueueSize() +"), getMinimumQueueSize("+ getMinimumQueueSize() +")");
+            }
+            if(sameCount == 180){
+                log.debug("WorkQueue-has-not-changed-for-long-time: getCurrentQueueSize("+ getCurrentQueueSize() +"), getMinimumQueueSize("+ getMinimumQueueSize() +")");
+                try{
+                    log.error("Will now try to force a shutdown... ");
+                    close();
+                } catch (Exception e){
+
+                } finally {
+                    System.exit(-1);
+                }
+            }
+
+
+        }
 
 
     }
@@ -51,6 +95,9 @@ public abstract class FlowVisitor<T extends ModelObjectInterface> {
     public abstract MQL.SelectQuery<T> query();
 
 
-    public abstract void process(T t);
+    public abstract boolean startFromBeginning();
+    public abstract void doWork(T t);
+    public abstract void close();
+    public abstract int getTotalCount();
 
 }
