1d3d585b64d9f78e4a03df9f9bbf96a1cd72dd60
Sebastian Berg <sebastian@less-is-more.dk>
2017-10-29 16:15:05 +0200

    We will not log out method calls that are less than 5 ms

--- nojpa_orm/src/main/java/dk/lessismore/nojpa/db/LimResultSet.java
+++ nojpa_orm/src/main/java/dk/lessismore/nojpa/db/LimResultSet.java
@@ -74,7 +74,7 @@ public class LimResultSet {
                     for (int j = listOfUnclosedSets.size() - 1; j >= 0; j--) {
                         LimResultSet limResultSet = listOfUnclosedSets.get(j);
                         if(limResultSet.creationDate.before(min2)){
-                            log.error("UNCLOSEDSETS(" + listOfUnclosedSets.size() +")["+ (limResultSet.creationDate.getTime()) +"] : " + (limResultSet.rawSqlStatement.replaceAll("\n", "")));
+                            log.error("UNCLOSEDSETS(" + listOfUnclosedSets.size() +")["+ (limResultSet.creationDate.getTime()) +"] : " + (limResultSet.rawSqlStatement.replaceAll("\n", " ")));
                             for(int i = 0; stackTraceElements != null && i < stackTraceElements.length; i++){
                                 log.error("UNCLOSEDSETS("+ listOfUnclosedSets.size() +") : " + stackTraceElements[i].getClassName() + "." + stackTraceElements[i].getMethodName() + ":" + stackTraceElements[i].getLineNumber());
 
