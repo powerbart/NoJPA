9f204107ffd474b062eec766f11247e3ce8bf061
Atanas Balevsky <atanas.balevsky@gmail.com>
2018-06-22 22:54:30 +0300

    fix required pathvariables & request params

--- nojpa_spring/src/main/java/dk/lessismore/nojpa/rest/NoJpaConverter.java
+++ nojpa_spring/src/main/java/dk/lessismore/nojpa/rest/NoJpaConverter.java
@@ -3,8 +3,10 @@ package dk.lessismore.nojpa.rest;
 import dk.lessismore.nojpa.reflection.db.model.ModelObjectInterface;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.core.convert.converter.GenericConverter;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.ParseException;
@@ -35,19 +37,20 @@ public class NoJpaConverter implements GenericConverter {
     @Override
     public Object convert(Object source, TypeDescriptor sourceType, TypeDescriptor targetType) {
         if (sourceType.getType().equals(String.class)) {
+            log.debug("Convert(source("+ source +"), sourceType("+ sourceType +"), targetType("+ targetType +"))");
+            boolean required = (targetType.hasAnnotation(RequestParam.class) && targetType.getAnnotation(RequestParam.class).required())
+                    || (targetType.hasAnnotation(PathVariable.class) && targetType.getAnnotation(PathVariable.class).required());
+
+            ModelObjectInterface moi = null;
             try {
-                log.debug("convert(source("+ source +"), sourceType("+ sourceType +"), targetType("+ targetType +"))");
-                return formatter.parse((String) source, null);
+                moi = formatter.parse((String) source, null);
             } catch (ParseException e) {
                 throw new RuntimeException(e);
-            } catch (RuntimeException re) {
-                // if @RequestParam(required=false)
-                if (targetType.hasAnnotation(RequestParam.class) && !targetType.getAnnotation(RequestParam.class).required()) {
-                    return null;
-                }
-                throw re;
             }
-
+            if (required && moi == null) {
+                throw new ConversionFailedException(sourceType, targetType, source, new RuntimeException());
+            }
+            return moi;
         } else {
             return formatter.print((ModelObjectInterface) source, null);
         }
