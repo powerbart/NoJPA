61c7a4b83225036b02bbb547606a6fb29d0cdcd3
Sebastaian Berg <sebastian@less-is-more.dk>
2019-07-16 18:06:46 +0300

    Optimizing

---
+++ nojpa_orm/src/main/java/dk/lessismore/nojpa/reflection/db/annotations/DontFollowField.java
@@ -0,0 +1,17 @@
+package dk.lessismore.nojpa.reflection.db.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created : with IntelliJ IDEA.
+ * User: seb
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DontFollowField {
+
+}
+
