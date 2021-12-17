c6ccb4b62caae812b2f06dd0241fbf4e5f4d8955
Sebastian Berg <sebastian@less-is-more.dk>
2021-12-17 23:21:19 +0200

    Minor

--- nojpa_common/src/main/java/dk/lessismore/nojpa/net/geo/GeoClient.java
+++ nojpa_common/src/main/java/dk/lessismore/nojpa/net/geo/GeoClient.java
@@ -63,9 +63,9 @@ public class GeoClient {
         }
         JSONObject json = new JSONObject(s);
         Geo geo = new Geo(
-                json.has("city") ? json.getString("city") : null,
-                json.has("country") ? json.getString("country") : null,
-                json.has("continent") ? json.getString("continent") : null
+                !json.isNull("city") ? json.getString("city") : null,
+                !json.isNull("country") ? json.getString("country") : null,
+                !json.isNull("continent") ? json.getString("continent") : null
         );
         synchronized (lookupCache) {
             lookupCache.put(ip, geo);
